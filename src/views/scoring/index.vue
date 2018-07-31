8fcb7a9604ecb4e8ca008660c3537737c1f5fc4d
MR-zn <846179626@qq.com>
2018-07-31 14:46:10 +0800

    v1.0.0

--- src/views/scoring/index.vue
+++ src/views/scoring/index.vue
@@ -18,8 +18,8 @@
                 </el-table-column>
                 <el-table-column prop="action" label="操作">
                     <template slot-scope="scope">
-                        <el-button type="success" plain  @click="handleEdit(scope.$index, scope.row)">加分</el-button>
-                        <el-button type="danger" plain @click="handleEdit(scope.$index, scope.row)">减分</el-button>
+                        <el-button type="success" plain @click="handleEdit(scope.$index, scope.row)">加分</el-button>
+                        <!-- <el-button type="danger" plain disabled>减分</el-button> -->
                     </template>
                 </el-table-column>
             </el-table>
@@ -40,12 +40,12 @@ export default {
             tableData: [
                 {
                     gname: "第一组",
-                    score: "90",
+                    score: "30",
                     color: "#337ab7"
                 },
                 {
                     gname: "第二组",
-                    score: "60",
+                    score: "30",
                     color: "#67C23A"
                 },
                 {
@@ -54,13 +54,13 @@ export default {
                     color: "#E6A23C"
                 },
                 {
-                    gname: "王小虎",
-                    score: "50",
+                    gname: "第四组",
+                    score: "30",
                     color: "#F56C6C"
                 },
                 {
-                    gname: "王小虎",
-                    score: "80",
+                    gname: "第五组",
+                    score: "30",
                     color: "#5bc0de"
                 }
             ]
@@ -68,10 +68,35 @@ export default {
     },
     methods: {
         handleEdit(index, row) {
-            console.log(index, row);
-        },
-        handleDelete(index, row) {
-            console.log(index, row);
+            this.$prompt("需要增加的分数（扣分则输入负数）", "提示", {
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                inputPattern: /\S/,
+                inputErrorMessage: "输入的内容不能为空"
+            })
+                .then(({ value }) => {
+                    row.score = parseFloat(row.score) + parseFloat(value);
+                    if (row.score > 100) {
+                        row.score = parseFloat(100);
+                        this.$message({
+                            message: "抱歉，分数最高为100分",
+                            type: "warning"
+                        });
+                    } else {
+                        if (true) {
+                            this.$message({
+                                type: "success",
+                                message: "增加了: " + value + "分"
+                            });
+                        }
+                    }
+                })
+                .catch(() => {
+                    this.$message({
+                        type: "info",
+                        message: "取消输入"
+                    });
+                });
         }
     }
 };
@@ -79,12 +104,15 @@ export default {
 
 
 <style lang="css">
-
 body {
     background: #f8f9fa;
 }
 
 .el-header {
     text-align: center;
+}
+
+.el-progress-bar__inner {
+    transition: all 0.3s;
 }
 </style>
